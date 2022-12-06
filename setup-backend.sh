21ac1df34776e38efc87d5580da29d15799201c3
Richard Hao <richard@rhao.io>
2022-12-06 18:32:14 +1100

    init

---
+++ setup-backend.sh
@@ -0,0 +1,9 @@
+[ -d "./packages" ] || mkdir packages
+
+cd packages
+repo init -u git@github.com:DelphinusLab/delphinus.git -b main -m delphinus-backend.xml
+repo sync
+cd -
+
+npm install
+npx lerna bootstrap --hoist --nohoist=circomlib
